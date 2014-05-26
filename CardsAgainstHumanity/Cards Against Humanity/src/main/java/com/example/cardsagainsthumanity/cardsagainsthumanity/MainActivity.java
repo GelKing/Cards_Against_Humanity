b28ff12a6acfe10294c99b15772d7420f2a5e730
Mcflubbin <mcflubbin@gmail.com>
2014-05-26 14:11:48 -0500

    Icons

--- CardsAgainstHumanity/Cards Against Humanity/src/main/java/com/example/cardsagainsthumanity/cardsagainsthumanity/MainActivity.java
+++ CardsAgainstHumanity/Cards Against Humanity/src/main/java/com/example/cardsagainsthumanity/cardsagainsthumanity/MainActivity.java
@@ -114,7 +114,8 @@ public class MainActivity extends Activity {
         // Upon interacting with UI controls, delay any scheduled hide()
         // operations to prevent the jarring behavior of controls going away
         // while interacting with the UI.
-        //findViewById(R.id.btnPlay).setOnTouchListener(mDelayHideTouchListener);
+        findViewById(R.id.play).setOnTouchListener(mDelayHideTouchListener);
+        findViewById(R.id.view).setOnTouchListener(mDelayHideTouchListener);
     }
 
     @Override
